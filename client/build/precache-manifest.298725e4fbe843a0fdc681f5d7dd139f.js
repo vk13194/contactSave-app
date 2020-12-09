1d9d97233f47dd02d7fccd53342e2d41c681f1e3
vk13194 <vijaym13194@gmail.com>
2020-12-09 10:16:13 +0530

    first deploy

---
+++ client/build/precache-manifest.298725e4fbe843a0fdc681f5d7dd139f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a991bd85f6eb488ce4aed0dadece23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d6d22fe7dd5293ed47b",
+    "url": "/static/css/main.2dae77fa.chunk.css"
+  },
+  {
+    "revision": "a197d9de69aa2550ab1f",
+    "url": "/static/js/2.efbcdc2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.efbcdc2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d6d22fe7dd5293ed47b",
+    "url": "/static/js/main.192def3f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,57 @@
+import React, {Fragment, useContext, useEffect} from 'react'
+import ContactContext from '../../context/contact/contactContext';
+//import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import ContactItem from './ContactItem'
+import Spinner from '../layout/Spinner'
+const Contacts = () => {
+    const contactContext = useContext(ContactContext)
+    const {contacts, filtered, getContacts, loading} = contactContext
+
+    useEffect(()=>{
+      getContacts();
+      //eslint-disable-next-line 
+    },[])
+
+    if(contacts !==null && contacts.length === 0 && !loading){
+        return <h3>please enter some contact</h3>
+    }
+    return (
+       <Fragment>
+         <div>
+           {contacts !==null && !loading ?(
+           <div>  {filtered !== null ? filtered.map(contact => 
+              (<ContactItem key={contact._id} contact={contact}/>)):
+              contacts.map(contact =>(
+               <ContactItem key={contact._id} contact={contact}/>
+           ))
+             }</div>
+           ):<Spinner />}
+         </div>
+       </Fragment>
+       /*<Fragment>
+           <TransitionGroup>
+          {filtered !== null
+            ? filtered.map(contact => (
+                <CSSTransition
+                  key={contact.id}
+                  timeout={500}
+                  classNames='item'
+                >
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              ))
+            : contacts.map(contact => (
+                <CSSTransition
+                  key={contact.id}
+                  timeout={500}
+                  classNames='item'
+                >
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              ))}
+        </TransitionGroup>
+       </Fragment>*/
+    )
+}
+
+export default Contacts
